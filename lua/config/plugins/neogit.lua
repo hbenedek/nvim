e85723600792cb2426b2d7b3eefa0ddb1eb9b22d
Benedek Harsanyi <benedek.harsanyi@juliusbaer.com>
2024-12-17 14:56:25 +0100

    add keymaps for neogit

--- lua/config/plugins/neogit.lua
+++ lua/config/plugins/neogit.lua
@@ -9,5 +9,22 @@ return {
     "ibhagwan/fzf-lua",              -- optional
     "echasnovski/mini.pick",         -- optional
   },
-  config = true
+  config = function()
+    vim.keymap.set("n", "<leader>gs", require("neogit").open)
+    vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>",
+      { silent = true, noremap = true }
+    )
+    vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>",
+      { silent = true, noremap = true }
+    )
+    vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>",
+      { silent = true, noremap = true }
+    )
+    vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>",
+      { silent = true, noremap = true }
+    )
+    vim.keymap.set("n", "<leader>gB", ":G blame<CR>",
+      { silent = true, noremap = true }
+    )
+  end
 }

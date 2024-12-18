e5281fb30f74808a4bfa734149783317ba20bc76
Benedek Harsanyi <benedek.harsanyi@juliusbaer.com>
2024-12-18 20:54:56 +0100

    github theme and formatting

--- init.lua
+++ init.lua
@@ -8,24 +8,33 @@ require("config.lazy")
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup('custom-open-term', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-  end
+	group = vim.api.nvim_create_augroup('custom-open-term', { clear = true }),
+	callback = function()
+		vim.opt.number = false
+	end
 })
 
-vim.keymap.set("n", "<leader>j", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 7)
-end
-)
+vim.keymap.set({ "n", "t" }, "<C-j>", function()
+	-- Get the current window's type
+	local buftype = vim.api.nvim_buf_get_option(0, "buftype")
+
+	-- If the current window is a terminal, close it
+	if buftype == "terminal" then
+		vim.cmd("close")
+	else
+		-- Otherwise, open a new terminal
+		vim.cmd.vnew()
+		vim.cmd.term()
+		vim.cmd.wincmd("J")
+		vim.api.nvim_win_set_height(0, 10)
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("i", true, false, true), "n", false)
+	end
+end)
--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,4 @@
+vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.cursorline = true

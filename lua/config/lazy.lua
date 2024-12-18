453f0ed7e3593e5559e5421b47fda55a219c7fa4
Benedek Harsanyi <benedek.harsanyi@juliusbaer.com>
2024-12-18 15:39:03 +0100

    add completion and mason

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -25,7 +25,7 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight" end },
+    { "folke/tokyonight.nvim",  config = function() vim.cmd.colorscheme "tokyonight" end },
     { import = "config.plugins" },
   },
 })
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -2,7 +2,20 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
+      {
+        'williamboman/mason.nvim',
+        opts = {
+          ensure_installed = {
+            "eslint-lsp",
+            "prettier",
+          },
+        },
+      },
+      'williamboman/mason-lspconfig.nvim',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/nvim-cmp',
       "folke/lazydev.nvim",
+      -- Automatically install LSPs to stdpath for neovim
       ft = "lua", -- only load on lua files
       opts = {
         library = {
@@ -13,8 +26,65 @@ return {
       },
     },
     config = function()
-      require("lspconfig").lua_ls.setup {}
+      require('mason').setup()
+      -- Enable the following language servers
+      local servers = { 'pyright', 'tsserver', 'lua_ls' }
+      require('mason-lspconfig').setup {
+        ensure_installed = servers,
+      }
+      -- Add cmp_nvim_lsp capabilities settings to lspconfig
+      -- This should be executed before you configure any language server
+      local lspconfig_defaults = require('lspconfig').util.default_config
+      lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lspconfig_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
 
+      -- This is where you enable features that only work
+      -- if there is a language server active in the file
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local map = function(keys, func, desc)
+            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        end,
+      })
+
+      -- Setup LSPs
+      for _, lsp in ipairs(servers) do
+        require('lspconfig')[lsp].setup {}
+      end
+
+
+      local cmp = require('cmp')
+
+      cmp.setup({
+        sources = {
+          { name = 'nvim_lsp' },
+        },
+        snippet = {
+          expand = function(args)
+            -- You need Neovim v0.10 to use vim.snippet
+            vim.snippet.expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({}),
+      })
+
+      --- autoformat on save
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
